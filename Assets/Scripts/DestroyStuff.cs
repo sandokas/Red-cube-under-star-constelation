0a39a4bdfa920e9cae073f8196f217f54aea29d1
SAPHETY\djsantos <david.santos@saphety.com>
2016-10-28 23:49:17 +0100

    adding dead state and animation
     added dead tag and destroy script

---
+++ Assets/Scripts/DestroyStuff.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AssemblyCSharp
+{
+	public class DestroyStuff : MonoBehaviour
+	{
+		public int scale = 10;
+		void OnCollisionEnter (Collision col) {
+			if (!col.gameObject.CompareTag("dead")) {
+				Destroy (col.gameObject);
+				for (int i = 0; i < scale; i++) {
+					GameObject temp = (GameObject) Instantiate (col.gameObject, col.gameObject.transform.position, Random.rotation);
+					temp.tag = "dead";
+					temp.transform.localScale = new Vector3 (1.0f / scale, 1.0f / scale, 1.0f / scale);
+				}
+			}
+		}
+	}
+}
+
